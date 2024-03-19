9606d20199bd7070ab075bb8d855fead23b01287
RajK-NeetiAI <raj@neeti.ai>
2024-03-19 16:45:49 +0530

    removed audio section

--- gradio_ui.py
+++ gradio_ui.py
@@ -2,7 +2,7 @@ import gradio as gr
 
 from conversation import *
 
-with gr.Blocks() as text_chat:
+with gr.Blocks(css='footer {visibility: hidden}') as demo:
     chatbot = gr.Chatbot(label='Values Knowledge', bubble_full_width=False)
     msg = gr.Textbox(label='Query', placeholder='Enter text and press enter')
     text_chat_clear = gr.ClearButton([msg, chatbot], variant='stop')
@@ -16,35 +16,5 @@ with gr.Blocks() as text_chat:
         [chatbot],
         [chatbot]
     )
-
-with gr.Blocks() as voice_chat:
-    chatbot = gr.Chatbot(label='Values Knowledge', bubble_full_width=False)
-    audio_msg = gr.Textbox(visible=False)
-
-    with gr.Row():
-        voice_chat_audio_file_path = gr.Audio(
-            sources=["microphone"], label='Record your query', type='filepath')
-        output_audio = gr.Audio(label='Chatbot response', type='filepath')
-
-    voice_chat_clear = gr.ClearButton(
-        [voice_chat_audio_file_path, chatbot, output_audio], variant='stop')
-
-    voice_chat_audio_file_path.stop_recording(
-        handle_user_voice_query,
-        [voice_chat_audio_file_path, chatbot],
-        [voice_chat_audio_file_path, chatbot]
-    ).then(
-        create_llm_conversation_audio,
-        [chatbot],
-        [chatbot, audio_msg]
-    ).then(
-        set_audio_output,
-        [audio_msg],
-        [output_audio]
-    )
-
-
-demo = gr.TabbedInterface([text_chat, voice_chat],
-                          ['Text Chat', 'Voice Chat'], css='footer {visibility: hidden}')
 
 demo.queue()
